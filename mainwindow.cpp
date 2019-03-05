6b13a69247f37d107fc30e2dffcb1eacc1b38450
bulbash <bulbash@vivaldi.com>
2019-03-05 15:20:53 -0500

    add treeView filesystem

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QDirModel>
+#include<QFileSystemModel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    showMaximized();
+//    QDirModel *model = new QDirModel();
+
+    QFileSystemModel *fileModel = new QFileSystemModel();
+    fileModel->setRootPath(QDir::currentPath());
+    fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Dirs);
+    ui->treeView->setModel(fileModel);
+
+    ui->treeView->setRootIndex(fileModel->index("/home"));
+    ui->treeView->hideColumn(1);
+    ui->treeView->hideColumn(2);
+    ui->treeView->hideColumn(3);
+
+//    ui->treeView->setRootIndex(model->index("/"));
 }
 
 MainWindow::~MainWindow()
