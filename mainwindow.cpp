b1b48fb3e8a11ba6e8f6ff789149cfb6297cc5f0
bulbash <bulbash@vivaldi.com>
2019-03-08 17:36:35 -0500

    complete v1.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<QDirModel>
-#include<QFileSystemModel>
+#include<QMessageBox>
+#include<QFileDialog>
+
+#include<QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,22 +13,166 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     showMaximized();
-//    QDirModel *model = new QDirModel();
 
-    QFileSystemModel *fileModel = new QFileSystemModel();
-    fileModel->setRootPath(QDir::currentPath());
-    fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Dirs);
-    ui->treeView->setModel(fileModel);
+    ui->textEdit->setFont(*font);
 
-    ui->treeView->setRootIndex(fileModel->index("/home"));
+    directoriesModel = new QFileSystemModel(this);
+    filesModel = new QFileSystemModel(this);
+
+    directoriesModel->setFilter(QDir::NoDotAndDotDot |QDir::Dirs);
+    directoriesModel->setRootPath("/home");
+    ui->treeView->setModel(directoriesModel);
+    ui->treeView->setRootIndex(directoriesModel->index("/home"));
     ui->treeView->hideColumn(1);
     ui->treeView->hideColumn(2);
     ui->treeView->hideColumn(3);
 
-//    ui->treeView->setRootIndex(model->index("/"));
+    filesModel->setFilter(QDir::Files);
+    filesModel->setRootPath("");
+
+    ui->listView->setModel(filesModel);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_treeView_clicked(const QModelIndex &index)
+{
+     currentPath = directoriesModel->fileInfo(index).absoluteFilePath();
+     ui->listView->setRootIndex(filesModel->setRootPath(currentPath));
+     QDir dir;
+}
+
+void MainWindow::on_listView_clicked(const QModelIndex &index)
+{
+    QModelIndexList templatelist = ui->listView->selectionModel()->selectedIndexes();
+
+    currentFileName=templatelist[0].data().toString();
+    openFile();
+}
+
+QString MainWindow::createFullName(){
+    return currentPath+"/"+currentFileName;
+}
+
+void MainWindow::openFile(QString name){
+
+    ui->textEdit->clear();
+
+    QFile file(name);
+
+    if(file.open(QIODevice::ReadWrite)){
+        QTextStream stream(&file);
+        stream.setCodec(codec);
+        QString str;
+        while(!stream.atEnd()){
+            str = stream.readLine();
+            ui->textEdit->append(str);
+        }
+
+        if(stream.status()!=QTextStream::Ok){
+            //TODO exception
+        }
+
+        file.close();
+    }
+}
+void MainWindow::openFile(){
+     openFile(createFullName());
+}
+
+void MainWindow::setCodec(QTextCodec *codec){
+    this->codec=codec;
+}
+
+
+void MainWindow::on_utf8_toggled(bool checked){
+    if(checked){
+        setCodec(QTextCodec::codecForName("UTF-8"));
+        openFile();
+    }
+}
+
+void MainWindow::on_cp1251_clicked(bool checked){
+    if(checked){
+        setCodec(QTextCodec::codecForName("cp1251"));
+        openFile();
+    }
+}
+
+void MainWindow::setFont(){
+    font->setBold(ui->bold->isChecked());
+    font->setItalic(ui->italic->isChecked());
+    font->setUnderline(ui->underline->isChecked());
+
+    QColor color(ui->colorComboBox->currentText());
+
+    ui->textEdit->setTextColor(color);
+    ui->textEdit->setFont(*font);
+
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox::about(this, "About TextHelper", "Simple text helper\nDesigned by Roman Frontov");
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    exit(0);
+}
+
+void MainWindow::on_resetButton_clicked()
+{
+    ui->bold->setChecked(false);
+    ui->italic->setChecked(false);
+    ui->underline->setChecked(false);
+
+    ui->utf8->setChecked(true);
+
+    ui->colorComboBox->setCurrentIndex(0);
+    setFont();
+
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString fileName=QFileDialog::getSaveFileName(this,
+            "Select file destination", "", "Text (*.txt);;All Files (*)");
+
+    QFile file(fileName);
+    if(file.open(QIODevice::WriteOnly)){
+        QTextStream stream(&file);
+        stream.setCodec(codec);
+
+        stream<< (ui->textEdit->toPlainText());
+        if(stream.status()!=QTextStream::Ok){
+            //TODO exception
+        }
+        file.close();
+
+    }
+}
+
+void MainWindow::on_colorComboBox_currentIndexChanged(const QString &arg1)
+{
+    setFont();
+}
+
+void MainWindow::on_bold_clicked()
+{
+    setFont();
+}
+
+void MainWindow::on_italic_clicked()
+{
+    setFont();
+}
+
+void MainWindow::on_underline_clicked()
+{
+    setFont();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include<QMainWindow>
+#include<QFileSystemModel>
+
+#include<QTextCodec>
+#include<QFont>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +19,51 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_treeView_clicked(const QModelIndex &index);
+
+    void on_listView_clicked(const QModelIndex &index);
+
+    void on_utf8_toggled(bool checked);
+
+    void on_cp1251_clicked(bool checked);
+
+    void on_actionAbout_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_resetButton_clicked();
+
+    void on_actionSave_As_triggered();
+
+    void on_colorComboBox_currentIndexChanged(const QString &arg1);
+
+    void on_bold_clicked();
+
+    void on_italic_clicked();
+
+    void on_underline_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QFileSystemModel * directoriesModel;
+    QFileSystemModel * filesModel;
+
+    QString currentPath;
+    QString currentFileName;
+
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+
+    QFont * font = new QFont("Times New Roman", 12);
+
+    void setCodec(QTextCodec * codec);
+
+    void setFont();
+
+    QString createFullName();
+
+    void openFile(QString name);
+    void openFile();
 };
 
 #endif // MAINWINDOW_H
